cd2ecb2e5ffa89fc48994757b138538f1c570808
n0miya <dulapah6@gmail.com>
2021-05-27 01:14:18 +0700

    Project Euler

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
-    int amount3 = 0;
-    int amount5 = 0;
-    int result3 = 0;
-
-    if (result3 < 10)
+    int amount;
+    for (int i = 1; i < 1000; i++)
     {
-        for (int i = 1; i < 10; i++)
+        if (i % 3 == 0 || i % 5 == 0)
         {
-            result3 = 3 * i;
-            amount3 += 1;
+            amount += i;
         }
     }
-    printf("%i\n", amount3);
+    printf("%i\n", amount);
 }

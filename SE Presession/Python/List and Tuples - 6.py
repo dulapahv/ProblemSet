3fbcbd2024c8475645ed9f1e6c53e1591a94b64a
n0miya <dulapah6@gmail.com>
2021-07-13 17:18:19 +0700

    Update List and Tuples - 6.py

--- SE Presession/Python/List and Tuples - 6.py
+++ SE Presession/Python/List and Tuples - 6.py
@@ -1,11 +1,20 @@
-UserInput = input("Input: ")
-SetInput = set(UserInput)
-ListInput = list(SetInput)
-result = []
-for i in range(len(ListInput)):
-    if ListInput[i] == "[" or ListInput[i] == "]" or ListInput[i] == "(" or ListInput[i] == ")" or ListInput[i] == "," or ListInput[i] == " ":
-        pass
-    else:
-        result.append(int(ListInput[i]))
+Input = [(1, 3, 4, 5), (4, 5, 7), (1, 2, 4)]
+sec = []
+for array in Input:
+    for nums in array:
+        if nums not in sec:
+            sec.append(nums)
+            sec.sort()
+print(sec)
 
-print(sorted(result))
+# UserInput = input("Input: ")
+# SetInput = set(UserInput)
+# ListInput = list(SetInput)
+# result = []
+# for i in range(len(ListInput)):
+#     if ListInput[i] == "[" or ListInput[i] == "]" or ListInput[i] == "(" or ListInput[i] == ")" or ListInput[i] == "," or ListInput[i] == " ":
+#         pass
+#     else:
+#         result.append(int(ListInput[i]))
+#
+# print(sorted(result))

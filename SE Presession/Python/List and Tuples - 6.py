21b1b3096e1a61f871a523c48019bddaae304047
n0miya <dulapah6@gmail.com>
2021-07-13 18:55:23 +0700

    Update List and Tuples - 6.py

--- SE Presession/Python/List and Tuples - 6.py
+++ SE Presession/Python/List and Tuples - 6.py
@@ -1,11 +1,16 @@
-Input = [(1, 3, 4, 5), (4, 5, 7), (1, 2, 4)]
-sec = []
-for array in Input:
-    for nums in array:
-        if nums not in sec:
-            sec.append(nums)
-            sec.sort()
-print(sec)
+num = []
+for char in input():
+    if char.isdigit(): num.append(int(char))
+print(list(set(num)))
+
+# Input = [(1, 3, 4, 5), (4, 5, 7), (1, 2, 4)]
+# sec = []
+# for array in Input:
+#     for nums in array:
+#         if nums not in sec:
+#             sec.append(nums)
+#             sec.sort()
+# print(sec)
 
 # UserInput = input("Input: ")
 # SetInput = set(UserInput)

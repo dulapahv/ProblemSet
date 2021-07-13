735b953188168f961be36b7ef4e675fae43ce162
n0miya <dulapah6@gmail.com>
2021-07-13 17:14:12 +0700

    Update List and Tuples - 2.py

--- SE Presession/Python/List and Tuples - 2.py
+++ SE Presession/Python/List and Tuples - 2.py
@@ -33,3 +33,43 @@ print(my_difference(list1, list2))
 #
 # my_difference = set(list1) - set(list2)
 # print(my_difference)
+
+# Another solution using loop
+# lst1 = [3, 1, 2, 7]
+# lst2 = [4, 1, 2, 5]
+#
+#
+# def my_union(lst1, lst2):
+#     union = []
+#     all_list = [lst1, lst2]
+#     for i in range(2):
+#         for members in all_list[i]:
+#             if members not in union:
+#                 union.append(members)
+#
+#     return union
+#
+#
+# def my_intersection(lst1, lst2):
+#     intersect = []
+#     for i in lst1:
+#         for j in lst2:
+#             if i == j and i not in intersect:
+#                 intersect.append(i)
+#
+#     return intersect
+#
+#
+# def my_difference(lst1, lst2):
+#     difference = []
+#     intersect = my_intersection(lst1, lst2)
+#     for member in lst1:
+#         if member not in intersect:
+#             difference.append(member)
+#
+#    return difference
+#
+#
+# print(my_union(lst1, lst2))
+# print(my_intersection(lst1, lst2))
+# print(my_difference(lst1, lst2))

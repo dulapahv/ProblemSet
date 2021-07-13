8c3fedb7854517012b3cf4b2b6662d49a34111ff
n0miya <dulapah6@gmail.com>
2021-07-13 17:17:29 +0700

    Update List and Tuples - 5.py

--- SE Presession/Python/List and Tuples - 5.py
+++ SE Presession/Python/List and Tuples - 5.py
@@ -1,6 +1,4 @@
-Tuple = (7, 8, 9)
-List = [1, 2, 3]
-newList = list(Tuple)
-CombinedList = newList + List
-Result = tuple(CombinedList)
-print(Result)
+Tuple = (7,8,9)
+list = [1,2,3]
+result = Tuple + tuple(list)
+print(result)

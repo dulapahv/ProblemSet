4b86c914003c910b67a212672fc468c5b83baeff
n0miya <dulapah6@gmail.com>
2021-07-15 10:58:56 +0700

    Update Sets and Dictionaries - 4.py

--- SE Presession/Python/Sets and Dictionaries - 4.py
+++ SE Presession/Python/Sets and Dictionaries - 4.py
@@ -1,13 +1,15 @@
-def FindPowerset(InputSet):
-    result = []
-    for i in range(2 ** len(InputSet)):
+def get_subsets(fullset):
+    listrep = list(fullset)
+    subsets = []
+    for i in range(2 ** len(listrep)):
         subset = []
-        for k in range(len(InputSet)):
+        for k in range(len(listrep)):
             if i & 1 << k:
-                subset.append(InputSet[k])
-        result.append(subset)
-    return result
+                subset.append(listrep[k])
+        subsets.append(subset)
+    return subsets
 
 
-set = FindPowerset([1, 2])
-print(set)
+subsets = get_subsets(set([1, 2, 3, ]))
+print(subsets)
+print(len(subsets))

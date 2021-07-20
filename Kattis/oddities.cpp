20f19fdf8549b1b4d890948ccc04538f14d3c759
n0miya <dulapah6@gmail.com>
2021-07-21 02:11:17 +0700

    Update

--- Kattis/oddities.cpp
+++ Kattis/oddities.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int cases, number[20];
-    scanf("%d", &cases);
+    cin >> cases;
 
     for (int i = 0; i < cases; i++) {
         scanf("%d", &number[i]);

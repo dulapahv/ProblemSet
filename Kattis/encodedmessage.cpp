279dba0f1e1d70c874b118a4a1b52975e6ed0cfd
n0miya <dulapah6@gmail.com>
2021-07-31 14:07:59 +0700

    Update

---
+++ Kattis/encodedmessage.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int cases;
+    cin >> cases;
+
+    string usin;
+    for (int i = 0; i < cases; i++) {
+        cin >> usin;
+
+        int x = sizeof(usin);
+    }
+
+}

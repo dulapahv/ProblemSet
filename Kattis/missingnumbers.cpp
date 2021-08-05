1a1b3dbffb5bd6498da32374f051d2bebf5aa294
n0miya <dulapah6@gmail.com>
2021-08-05 23:08:00 +0700

    Failed

--- Kattis/missingnumbers.cpp
+++ Kattis/missingnumbers.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int usin, count = 0;
-    while (cin >> usin) {
-        count++;
+    int cases;
+    cin >> cases;
+
+    int usin[cases];
+    for (int i = 0; i < cases; i++) {
+        cin >> usin[i];
     }
-    cout << count << endl;
+
+    int n = sizeof(usin)/sizeof(usin[0]);
+    sort(usin, usin + n);
+
+    vector<int> result;
+    for (int i = 0; i < cases - 1; i++) {
+        if (usin[i] != i + 1) {
+            result.push_back(i + 1);
+        }
+    }
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << endl;
+    }
 }

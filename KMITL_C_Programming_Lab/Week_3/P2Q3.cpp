3a5760e4ed33b7bc9695fac28a1b010c133d4290
n0miya <dulapah6@gmail.com>
2021-08-18 00:49:48 +0700

    Update

--- KMITL_C_Programming_Lab/Week_3/P2Q3.cpp
+++ KMITL_C_Programming_Lab/Week_3/P2Q3.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() {
     int height;
     cin >> height;
+
+    if (height % 2 == 0)
+        height = (height / 2) + 1; 
+    else
+        height = floor(height / 2);
+
     int space = height - 1;
     for (int i = 1; i <= height; i++) {
         for (int j = 1; j <= space; j++)
--- KMITL_C_Programming_Lab/Week_3/Q3.cpp
+++ KMITL_C_Programming_Lab/Week_3/Q3.cpp
@@ -6,10 +6,11 @@ int main() {
     int height;
     cin >> height;
 
-    for (int i = 0; height < height; i++) {
+    for (int i = 0; i < height; i++) {
         for (int j = 0; j < height - i; j++) {
-            cout << "*";
+            cout << " ";
         }
+        
         cout << endl;
     }
 }

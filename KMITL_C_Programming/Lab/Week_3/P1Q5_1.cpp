09eba71316278628ee8f29604875d1c1edd299da
n0miya <dulapah6@gmail.com>
2021-08-18 10:42:43 +0700

    Changed directory

--- KMITL_C_Programming/Lab/Week_3/P1Q5_1.cpp
+++ KMITL_C_Programming/Lab/Week_3/P1Q5_1.cpp
@@ -3,15 +3,15 @@
 
 using namespace std;
 
-void to_lower(string *str) {
-    for (int i = 0; i < str->length(); i++) {
-        to_lower(str[i]);
-    }
+void to_lower(string *strPtr) {
+    string temp = *strPtr;
+    *strPtr = ;
 }
 
 int main() {
     string usin;
     getline(cin, usin);
+
     to_lower(&usin);
     cout << usin << endl;
 }

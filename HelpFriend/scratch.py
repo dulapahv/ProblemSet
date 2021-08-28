0ab3e8ee5dd5640a6ecf89701f7d46f210e42372
n0miya <dulapah6@gmail.com>
2021-08-28 13:09:47 +0700

    Update scratch.py

--- HelpFriend/scratch.py
+++ HelpFriend/scratch.py
@@ -1,30 +1,3 @@
-
-# Import module 
-from tkinter import *
-    
-# Create object 
-splash_root = Tk() 
-    
-# Adjust size 
-splash_root.geometry("200x200")
-  
-# Set Label
-splash_label = Label(splash_root,text="Splash Screen",font=18)
-splash_label.pack()
-  
-# main window function
-def main():  
-    # destory splash window
-    splash_root.destroy()
-  
-    # Execute tkinter 
-    root = Tk() 
-        
-    # Adjust size 
-    root.geometry("400x400") 
-  
-# Set Interval
-splash_root.after(3000,main)
-  
-# Execute tkinter 
-mainloop()
+d = 10
+n = 15
+print("the value with", n ,"% will be",d)

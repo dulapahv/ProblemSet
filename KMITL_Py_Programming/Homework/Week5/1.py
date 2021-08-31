081747dc6ded76ed2e46cc8352d3e2c7e49e48ff
n0miya <dulapah6@gmail.com>
2021-08-31 12:22:57 +0700

    Refactored code

--- KMITL_Py_Programming/Homework/Week5/1.py
+++ KMITL_Py_Programming/Homework/Week5/1.py
@@ -21,9 +21,9 @@ p1x, p1y = (input("Enter x,y coordinate for P1: ")).split(",")
 t.setpos(float(p1x) * 10, float(p1y) * 10)
 t.write(f"P1 ({p1x}, {p1y})", font=("Arial", 14))
 
+# orient the arrow for reference direction to determine left/right
 t.shapesize(1.5)
 angle = math.degrees(math.atan((float(p1y) - float(p0y)) / (float(p1x) - float(p0x))))
-# orient the arrow for left/right reference
 if (float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) < 0) or (float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) > 0):
     t.lt(angle + 180)
 else:
@@ -46,16 +46,12 @@ if ((float(p2y) - float(p0y)) >= (-1 / m) * ((float(p2x) - float(p0x))) and (flo
         msg = "P2 is on the line between P0 and P1."
     # determine the location of P2
     elif (float(p2y) - float(p0y)) > m * ((float(p2x) - float(p0x))) :
-        if float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) < 0:
-            msg = "P2 is on the right side of the line between P0 and P1."
-        elif float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) > 0:
+        if (float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) < 0) or (float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) > 0):
             msg = "P2 is on the right side of the line between P0 and P1."
         else:
             msg = "P2 is on the left side of the line between P0 and P1."
     elif (float(p2y) - float(p0y)) < m * ((float(p2x) - float(p0x))) :
-        if float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) < 0:
-            msg = "P2 is on the left side of the line between P0 and P1."
-        elif float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) > 0:
+        if (float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) < 0) or (float(p1x) - float(p0x) < 0 and float(p1y) - float(p0y) > 0):
             msg = "P2 is on the left side of the line between P0 and P1."
         else:
             msg = "P2 is on the right side of the line between P0 and P1."

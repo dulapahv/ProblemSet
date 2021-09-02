45ae5f094e27daa3690cb6ea48f7dc74b07bd6dc
n0miya <dulapah6@gmail.com>
2021-09-02 12:52:04 +0700

    Refactored code

--- KMITL_CPP_Programming/Activity4/1.cpp
+++ KMITL_CPP_Programming/Activity4/1.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    int c;
-    vector<int> input;
-    while((c = getchar()) != EOF) {
-        printf("%c", char(c));
-        input.push_back(char(c));
+    int c, zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
+    printf("Enter a text: ");
+    while (c != '\n') {
+        scanf("%c", &c);
+        switch (c) {
+            case '0':
+                zero++;
+                break;
+            case '1':
+                one++;
+                break;
+            case '2':
+                two++;
+                break;
+            case '3':
+                three++;
+                break;
+            case '4':
+                four++;
+                break;
+            case '5':
+                five++;
+                break;
+            case '6':
+                six++;
+                break;
+            case '7':
+                seven++;
+                break;
+            case '8':
+                eight++;
+                break;
+            case '9':
+                nine++;
+                break;
     }
 }
--- KMITL_CPP_Programming/Activity4/2_1.cpp
+++ KMITL_CPP_Programming/Activity4/2_1.cpp
@@ -1,42 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int c, zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
+    int c, number[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     printf("Enter a text: ");
     while ((c = getchar()) != EOF) {
-        switch (c) {
-            case '0':
-                zero++;
-                break;
-            case '1':
-                one++;
-                break;
-            case '2':
-                two++;
-                break;
-            case '3':
-                three++;
-                break;
-            case '4':
-                four++;
-                break;
-            case '5':
-                five++;
-                break;
-            case '6':
-                six++;
-                break;
-            case '7':
-                seven++;
-                break;
-            case '8':
-                eight++;
-                break;
-            case '9':
-                nine++;
-                break;
-        }    
+        number[c - '0']++;
     }
     printf("Digit:       0 1 2 3 4 5 6 7 8 9\n");
-    printf("Occurrences: %d %d %d %d %d %d %d %d %d %d\n", zero, one, two, three, four, five, six, seven, eight, nine);
+    printf("Occurrences: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", number[i]);
+    }
 }

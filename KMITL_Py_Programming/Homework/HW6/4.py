22c231a2fbdba93ba68346aed6c29c8bec040ea7
n0miya <dulapah6@gmail.com>
2021-09-16 00:37:33 +0700

    Fix HW

--- KMITL_Py_Programming/Homework/HW6/4.py
+++ KMITL_Py_Programming/Homework/HW6/4.py
@@ -1,36 +1,29 @@
 number = int(input("Enter a number: "))
 numArr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-numArr1 = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"] # -1
-numArr2 = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"] # -1
+numArr1 = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+numArr2 = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
-if 0 <= number <= 9:
+if number <= 9:
     print(f"{numArr[number]}")
-elif 10 <= number <= 19:
-    if number == 10:
-        print(f"{numArr2[0]}")
-    else:
-        print(f"{numArr1[int(str(number)[1]) - 1]}")
-elif 20 <= number <= 99:
-    if number % 10 == 0:
-        print(f"{numArr2[int(str(number)[0]) - 1]}")
-    else:
-        print(f"{numArr2[int(str(number)[0]) - 1]}-{numArr[int(str(number)[1])]}") 
-elif 100 <= number <= 109:
-    if number == 100:
-        print(f"{numArr[1]} hundred")
+elif 11 <= number <= 19:
+    print(f"{numArr1[int(str(number)[1]) - 1]}")
+elif int(str(number)[1]) == 0 and number <= 99:
+    print(f"{numArr2[int(str(number)[0]) - 1]}")
+elif 21 <= number <= 99:
+    print(f"{numArr2[int(str(number)[0]) - 1]}-{numArr[int(str(number)[1])]}") 
+elif int(str(number)[1]) == 0:
+    if int(str(number)[2]) == 0:
+        print(f"{numArr[int(str(number)[0])]} hundred")
     else:
         print(f"{numArr[int(str(number)[0])]} hundred and {numArr[int(str(number)[2])]}")
-elif int(str(number)[1]) == 1:
-    if number == 110:
-        print(f"{numArr[int(str(number)[0])]} hundred and {numArr2[0]}")
-    else:
+elif 111 <= number <= 119:
+    print(f"{numArr[int(str(number)[0])]} hundred and {numArr1[int(str(number)[2]) - 1]}")
+elif int(str(number)[2]) == 0 and number <= 999:
+    print(f"{numArr[int(str(number)[0])]} hundred and {numArr2[int(str(number)[1]) - 1]}")
+elif 121 <= number <= 999:
+    if int(str(number)[1]) == 1:
         print(f"{numArr[int(str(number)[0])]} hundred and {numArr1[int(str(number)[2]) - 1]}")
-elif 120 <= number <= 999:
-        if number % 10 == 0:
-            print(f"{numArr[int(str(number)[0])]} hundred and {numArr2[int(str(number)[1]) - 1]}")
-        elif int(str(number)[1]) == 0:
-            print(f"{numArr[int(str(number)[0])]} hundred and {numArr[int(str(number)[2])]}")
-        else:
-            print(f"{numArr[int(str(number)[0])]} hundred and {numArr2[int(str(number)[1]) - 1]}-{numArr[int(str(number)[2])]}")
+    else:
+        print(f"{numArr[int(str(number)[0])]} hundred and {numArr2[int(str(number)[1]) - 1]}-{numArr[int(str(number)[2])]}")
 else:
     print("I don't know.")

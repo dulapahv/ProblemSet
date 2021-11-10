ec1d0f88578679042769fb0b1c0da88f018e2a5b
Dulapah Vibulsanti <dulapah6@gmail.com>
2021-11-10 12:09:48 +0700

    Update 8.c

--- HelpFriend/Boat/8.c
+++ HelpFriend/Boat/8.c
@@ -10,13 +10,16 @@ int main() {
     if ((pos=strchr(input, '\n')) != NULL)
     *pos = '\0';
 
-    int count = 0;
-    for (int i = 0; i < sizeof(input)/sizeof(input[0]); i++) {
-        if ((input[i] < 'A' || input[i] >  'Z') && (input[i] < 'a' || input[i] > 'z') && (input[i] < 0 || input[i] > 9) && input[i] != ' ') {
-            input[i] = ' ';
-            count++;
+    char result[SIZE] = {};
+    int count = 0, index = 0;
+    for (int i = 0; i < strlen(input); i++) {
+        if ((input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z') || (input[i] >= '0' && input[i] <= '9') || (input[i] == ' ')) {
+            result[index] = input[i];
+            index++;
         }
+        else
+            count++;
     }
 
-    printf("%d %s", count, input);
+    printf("%d %s", count, result);
 }

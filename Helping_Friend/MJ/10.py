ddec4adcdc4193a980626903cebf083931c29b0e
Dulapah Vibulsanti <dulapah6@gmail.com>
2021-11-21 00:41:30 +0700

    Update

--- Helping_Friend/MJ/10.py
+++ Helping_Friend/MJ/10.py
@@ -1,10 +1,10 @@
 def LCS(s1, s2):
-       temp = []
-   for i in range(len(s1)):
-       j = len(s1)
-       while (j > 0):
-           if s1[i:j] in s2:
-               if len(temp) <= len(s1[i:j]):
-                   temp = s1[i:j]
-           j -= 1
-   return temp
+    temp = []
+    for i in range(len(s1)):
+        j = len(s1)
+        while (j > 0):
+            if s1[i:j] in s2:
+                if len(temp) <= len(s1[i:j]):
+                    temp = s1[i:j]
+            j -= 1
+    return temp

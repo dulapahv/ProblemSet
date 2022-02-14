23568a70d1569801edbffdfca23eb73afe920f91
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-02-14 18:52:57 +0700

    Completed first task
    - Remove debug code
    - Remove Town class and code
    - Add suffix to output
    - Fixed precision to 9 decimal points

--- KMITL_C-CPP/Semester_2/Lab/Lab4/main.cpp
+++ KMITL_C-CPP/Semester_2/Lab/Lab4/main.cpp
@@ -43,92 +43,21 @@ int main() {
 			townSubGroup.push_back(town);
 			town.clear();
 		}
-		else if (strcmp(type, "Town") == 0) {
-			town.push_back(Town(tempKey, tempName, tempLatitude, tempLongitude, tempPopulation, tempArea));
-		}
 	}
 	/* Push the last sub_district and town vector into sub_districtGroup and townGroup vector */
 	sub_districtGroup.push_back(sub_district);
-	townSubGroup.push_back(town);
-	townGroup.push_back(townSubGroup);
-	
 
 	/* Remove the first index of a sub_districtGroup and townGroup vector */
 	sub_districtGroup.erase(sub_districtGroup.begin());
-	townSubGroup.erase(townSubGroup.begin());
-	townGroup.erase(townGroup.begin());
 
-	/* Prevent auto conversion of output number to exponential */
-	cout << setprecision(12);
-
-	/*** DEBUG ***/
-	///* Get name of each province, sub-district, and town */
-	//cout << "\nProvince:" << endl;
-	//for (unsigned int i = 0; i < province.size(); i++)
-	//	cout << province[i].getName() << endl;
-	//cout << "\nSub-District:" << endl;
-	//for (unsigned int i = 0; i < sub_district.size(); i++)
-	//	cout << sub_district[i].getName() << endl;
-	//cout << "\nTown:" << endl;
-	//for (unsigned int i = 0; i < town.size(); i++)
-	//	cout << town[i].getName() << endl;
-
-	///* Get sum of population in each province, sub-district, and town */
-	//double sumProvince = 0, sumSub = 0, sumTown = 0;
-	//for (unsigned int i = 0; i < province.size(); i++)
-	//	sumProvince += province[i].getPopulation();
-	//for (unsigned int i = 0; i < sub_district.size(); i++)
-	//	sumSub += sub_district[i].getPopulation();
-	//for (unsigned int i = 0; i < town.size(); i++)
-	//	sumTown += town[i].getPopulation();
-
-	//cout << "\nSum Province: " << sumProvince << endl;
-	//cout << "Sum Sub-District: " << sumSub << endl;
-	//cout << "Sum Town: " << sumTown << endl;
-
-	///* Get area of each province, sub-district, and town */
-	//double sumProvinceArea = 0, sumSubArea = 0, sumTownArea = 0;
-	//for (unsigned int i = 0; i < province.size(); i++)
-	//	sumProvinceArea += province[i].getArea();
-	//for (unsigned int i = 0; i < sub_district.size(); i++)
-	//	sumSubArea += sub_district[i].getArea();
-	//for (unsigned int i = 0; i < town.size(); i++)
-	//	sumTownArea += town[i].getArea();
-
-	//cout << "\nProvince Area: " << sumProvinceArea << endl;
-	//cout << "Sub-District Area: " << sumSubArea << endl;
-	//cout << "Sum Town Area: " << sumTownArea << endl;
-
-	///* Get population density of each province, sub-district, and town */
-	//cout << "\nProvince population density:" << endl;
-	//for (unsigned int i = 0; i < province.size(); i++)
-	//	cout << province[i].getName() << ": " << province[i].getPopulationDensity() << endl;
-	//cout << "\nSub-District population density:" << endl;
-	//for (unsigned int i = 0; i < sub_district.size(); i++)
-	//	cout << sub_district[i].getName() << ": " << sub_district[i].getPopulationDensity() << endl;
-	//cout << "\nTown population density:" << endl;
-	//for (unsigned int i = 0; i < town.size(); i++)
-	//	cout << town[i].getName() << ": " << town[i].getPopulationDensity() << endl;
-
-	///* Display population density of each province, sub-district, and town */
-	//for (unsigned int i = 0; i < province.size(); i++)
-	//	cout << "Province " << province[i].getName() << setw(10) << '\t' << province[i].getPopulationDensity() << endl;
-	//for (unsigned int i = 0; i < sub_district.size(); i++)
-	//	cout << "- " << sub_district[i].getName() << setw(10) << '\t' << sub_district[i].getPopulationDensity() << endl;
-	//for (unsigned int i = 0; i < town.size(); i++)
-	//	cout << "\t- " << town[i].getName() << setw(10) << '\t' << town[i].getPopulationDensity() << endl;
-	/*** DEBUG ***/
+	/* Round number */
+	cout << setprecision(9) << fixed;
 
 	/* Main program part */
 	for (unsigned int i = 0; i < province.size(); i++) {
-		cout << "Province " << province[i].getName() << setw(13) << '\t' << province[i].getPopulationDensity() << endl;
-		for (unsigned int j = 0; j < sub_districtGroup[i].size(); j++) {
-			cout << "- " << sub_districtGroup[i][j].getName() << setw(18) << '\t' << sub_districtGroup[i][j].getPopulationDensity() << endl;
-			for (unsigned int k = 0; k < townGroup[i][j].size(); k++) {
-				cout << "\t- " << townGroup[i][j][k].getName() << setw(10) << '\t' << townGroup[i][j][k].getPopulationDensity() << endl;
-				cout << i << j << k;
-			}	
-		}
+		cout << "Province " << province[i].getName() << setw(13) << '\t' << province[i].getPopulationDensity() << " people/sq.m." << endl;
+		for (unsigned int j = 0; j < sub_districtGroup[i].size(); j++)
+			cout << "- " << sub_districtGroup[i][j].getName() << setw(18) << '\t' << sub_districtGroup[i][j].getPopulationDensity() << " people/sq.m." << endl;
 		cout << endl;
 	}
 

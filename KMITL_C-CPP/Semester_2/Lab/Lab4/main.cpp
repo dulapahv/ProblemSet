6d5878ad6d64cb054d9745b138a4023d1bb9d912
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-02-14 11:30:24 +0700

    Add debug output

--- KMITL_C-CPP/Semester_2/Lab/Lab4/main.cpp
+++ KMITL_C-CPP/Semester_2/Lab/Lab4/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,7 +11,7 @@
 using namespace std;
 
 int main() {
-    /** Reads input file **/
+    /* Reads input file */
     ifstream inFile(INPUT);
 
     vector<Region> region;
@@ -27,13 +28,60 @@ int main() {
     while (inFile >> tempKey >> tempName >> tempLatitude >> tempLongitude >> tempPopulation >> tempArea) {
         /* Store data according to the region type */
         const char * type = tempKey.c_str(); // Convert string to const char *
-        if (strcmp(type, "Province"))
+        if (strcmp(type, "Province") == 0)
             province.push_back(Province(tempKey, tempName, tempLatitude, tempLongitude, tempPopulation, tempArea));
-        else if (strcmp(type, "Sub_District"))
+        else if (strcmp(type, "Sub") == 0)
             sub_district.push_back(Sub_District(tempKey, tempName, tempLatitude, tempLongitude, tempPopulation, tempArea));
-        else if (strcmp(type, "Town")) {
+        else if (strcmp(type, "Town") == 0)
             town.push_back(Town(tempKey, tempName, tempLatitude, tempLongitude, tempPopulation, tempArea));
     }
+    
+    /** DEBUG **/
+    /* Get name of each province and sub-district */
+    cout << "\nProvince:" << endl;
+    for (int i = 0; i < province.size(); i++)
+        cout << province[i].getName() << endl;
+    cout << "\nSub-District:" << endl;
+    for (int i = 0; i < sub_district.size(); i++)
+        cout << sub_district[i].getName() << endl;
+    for (int i = 0; i < town.size(); i++)
+        cout << town[i].getName() << endl;
+    
+    /* Get sum of population in each province */
+    double sumProvince = 0, sumSub = 0, sumTown = 0;
+    for (int i = 0; i < province.size(); i++)
+        sumProvince += province[i].getPopulation();
+    for (int i = 0; i < sub_district.size(); i++)
+        sumSub += sub_district[i].getPopulation();
+    for (int i = 0; i < town.size(); i++)
+        sumTown += town[i].getPopulation();
+
+    cout << setprecision(12) << "\nSum Province: " << sumProvince << endl;
+    cout << "Sum Sub-District: " << sumSub << endl;
+    cout << "Sum Town: " << sumTown << endl;
+
+    /* Get area of each province */
+    double sumProvinceArea = 0, sumSubArea = 0, sumTownArea = 0;
+    for (int i = 0; i < province.size(); i++)
+        sumProvinceArea += province[i].getArea();
+    for (int i = 0; i < sub_district.size(); i++)
+        sumSubArea += sub_district[i].getArea();
+    for (int i = 0; i < town.size(); i++)
+        sumTownArea += town[i].getArea();
+    
+    cout << "\nProvince Area: " << sumProvinceArea << endl;
+    cout << "Sub-District Area: " << sumSubArea << endl;
+    cout << "Sum Town Area: " << sumTownArea << endl;
+
+    /* Get population density of each province and sub-district */
+    cout << "\nProvince population density:" << endl;
+    for (int i = 0; i < province.size(); i++)
+        cout << province[i].getName() << ": " << province[i].getPopulationDensity() << endl;
+    cout << "\nSub-District population density:" << endl;
+    for (int i = 0; i < sub_district.size(); i++)
+        cout << sub_district[i].getName() << ": " << sub_district[i].getPopulationDensity() << endl;
+    for (int i = 0; i < town.size(); i++)
+        cout << town[i].getName() << ": " << town[i].getPopulationDensity() << endl;
 
     inFile.close();
     return 0;

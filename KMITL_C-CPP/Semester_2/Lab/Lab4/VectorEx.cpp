e9002ec06a72c9986853cab0a408de11d1a54022
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-02-17 18:22:59 +0700

    Added execution timer

--- KMITL_C-CPP/Semester_2/Lab/Lab4/VectorEx.cpp
+++ KMITL_C-CPP/Semester_2/Lab/Lab4/VectorEx.cpp
@@ -6,37 +6,77 @@
 #include "Rectangle.h"
 #include "Complex.h"
 #include "Point.h"
+#include <chrono>
 #include <vector> // for vectors
-
 
 const int n_max = 10000;
 
-int main()
-{
-    std::vector<Rectangle> vr;
-    Point p1, p2;
-    p1 = Point(1.0, 1.0);
-    p2 = Point(2.0, 4.0);
-    Rectangle ra(p1, p2);
+int main() {
+	std::vector<Rectangle> vr;
+	Point p1, p2;
+	p1 = Point(1.0, 1.0);
+	p2 = Point(2.0, 4.0);
+	Rectangle ra(p1, p2);
 
-    std::cout << "Make a vector " << std::endl;
+	std::cout << "Make a vector " << std::endl;
 
-    size_t n_current = vr.size();
-    size_t n_cap = vr.capacity();
-    size_t max_size = 0;
-    std::cout << "Init elem " << n_current << " cap " << n_cap << std::endl;
-    //for (int j = 0; j < n_max; j++) {
-    //    // Add some code here to show us how vector allocates space on YOUR computer!!
-    //}
-    //// Print some conclusions here .. how many extra elements are added
-    //// whenever the vector becomes full
+	size_t n_current = vr.size();
+	size_t n_cap = vr.capacity();
+	size_t max_size = 0;
+	std::cout << "Init elem " << n_current << " cap " << n_cap << std::endl;
 
-    //// Try using reserve here to see what happens?  Can you adjust the
-    //// extra space allocated ??
-    //for (int j = 0; j < n_max; j++) {
-    //    // Now show us how vector allocates space on YOUR computer after reserve
-    //}
+	std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
 
+	for (int j = 0; j < n_max; j++) {
+		// Add some code here to show us how vector allocates space on YOUR computer
+		vr.push_back(ra);
+		std::cout << "Added 1 element. Size: " << vr.size() << " | Capacity: " << vr.capacity() << std::endl;
+	}
+
+	std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> time_first = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+
+	// Print some conclusions here .. how many extra elements are added
+	// whenever the vector becomes full
+	std::cout << "\nCONCLUSION - Inserting " << n_max << " elements WITHOUT reserve" << std::endl;
+	std::cout << "Size of vector: " << vr.size() << std::endl;
+	std::cout << "Capacity of vector: " << vr.capacity() << std::endl;
+	std::cout << "Time: " << time_first.count() * 1000 << " milliseconds." << std::endl;
+	system("pause");
+
+	// Try using reserve here to see what happens?  Can you adjust the
+	// extra space allocated ??
+	vr.clear();
+	vr.shrink_to_fit();
+	vr.reserve(n_max);
+
+	std::chrono::steady_clock::time_point t3 = std::chrono::steady_clock::now();
+
+	for (int j = 0; j < n_max; j++) {
+		// Now show us how vector allocates space on YOUR computer after reserve
+		vr.push_back(ra);
+		std::cout << "Added 1 element. Size: " << vr.size() << " | Capacity: " << vr.capacity() << std::endl;
+	}
+
+	std::chrono::steady_clock::time_point t4 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> time_second = std::chrono::duration_cast<std::chrono::duration<double>>(t4 - t3);
+
+	std::cout << "\nCONCLUSION - Inserting " << n_max << " elements WITH reserve" << std::endl;
+	std::cout << "Size of vector: " << vr.size() << std::endl;
+	std::cout << "Capacity of vector: " << vr.capacity() << std::endl;
+	std::cout << "Time: " << time_second.count() * 1000 << " milliseconds" << std::endl;
+
+	std::cout << "\n==================================================" << std::endl;
+	std::cout << "[SUMMARY]" << std::endl;
+	std::cout << "\nInserting " << n_max << " elements WITHOUT reserve" << std::endl;
+	std::cout << "Size of vector: " << vr.size() << std::endl;
+	std::cout << "Capacity of vector: " << vr.capacity() << std::endl;
+	std::cout << "Time: " << time_first.count() * 1000 << " milliseconds." << std::endl;
+
+	std::cout << "\nInserting " << n_max << " elements WITH reserve" << std::endl;
+	std::cout << "Size of vector: " << vr.size() << std::endl;
+	std::cout << "Capacity of vector: " << vr.capacity() << std::endl;
+	std::cout << "Time: " << time_second.count() * 1000 << " milliseconds" << std::endl;
+
+	system("pause");
 }
-
-

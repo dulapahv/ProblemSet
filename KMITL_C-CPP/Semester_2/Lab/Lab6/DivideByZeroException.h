f43626d304236ef723dda4ad63d55d4c99649e3c
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-03-03 19:38:08 +0700

    Create DivideByZeroException header

---
+++ KMITL_C-CPP/Semester_2/Lab/Lab6/DivideByZeroException.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+//user defined class for handling exception
+class Exception : public runtime_error {
+public:
+	Exception()
+		: runtime_error("Math error: Attempted to divide by Zero\n") {
+	}
+};

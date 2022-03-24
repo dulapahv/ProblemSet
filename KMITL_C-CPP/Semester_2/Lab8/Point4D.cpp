de5b26ffd2ff6eab83189a8b1f7f57499f55c5c4
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-03-24 17:23:25 +0700

    Turn Point4D into a template class and add operator methods

--- KMITL_C-CPP/Semester_2/Lab8/Point4D.cpp
+++ KMITL_C-CPP/Semester_2/Lab8/Point4D.cpp
@@ -7,10 +7,16 @@ using namespace std;
 
 #include "Point4D.h"
 
-void Point4D::print(ofstream& f) {
+template <typename T>
+void Point4D<T>::print(ofstream& f) {
 	f << '(' << getX() << ',' << getY() << ',' << z4 << ')';
 }
-void Point4D::print() {
+
+template <typename T>
+void Point4D<T>::print() {
 	cout << '(' << getX() << ', ' << getY() << ', ' << z4 << ')';
 }
 
+template class Point4D<int>;
+template class Point4D<float>;
+template class Point4D<double>;
--- KMITL_C-CPP/Semester_2/Lab8/Point4D.h
+++ KMITL_C-CPP/Semester_2/Lab8/Point4D.h
@@ -7,6 +7,7 @@ using namespace std;
 
 #include "Point3D.h"
 
+template <typename T>
 class Point4D : public Point3D {
 private:
 	double z4;
@@ -14,6 +15,27 @@ public:
 	Point4D(double x, double y, double z, double z4) : Point3D(x, y, z) {
 		this->z4 = z4;
 	}
+
+	/* + operator method */
+	Point4D<T> operator+(Point4D<T> p) {
+		return Point4D<T>(getX() + p.getX(), getY() + p.getY(), getZ() + p.getZ(), z4 + p.z4);
+	}
+
+	/* - operator method */
+	Point4D<T> operator-(Point4D<T> p) {
+		return Point4D<T>(getX() - p.getX(), getY() - p.getY(), getZ() - p.getZ(), z4 - p.z4);
+	}
+
+	/* * operator method */
+	Point4D<T> operator*(double scale) {
+		return Point4D<T>(getX() * scale, getY() * scale, getZ() * scale, z4 * scale);
+	}
+
+	/* / operator method */
+	Point4D<T> operator/(double scale) {
+		return Point4D<T>(getX() / scale, getY() / scale, getZ() / scale, z4 / scale);
+	}
+
 
 	void print(ofstream& f);
 	void print();

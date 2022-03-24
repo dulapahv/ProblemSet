daa19cb54c56f7d40e782ea82fcbad0e1768921c
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-03-24 19:43:04 +0700

    Re-implement operator methods and add exceptions

--- KMITL_C-CPP/Semester_2/Lab8/Point4D.cpp
+++ KMITL_C-CPP/Semester_2/Lab8/Point4D.cpp
@@ -2,10 +2,11 @@
 
 #include <iostream>
 #include "math.h"
+#include "Point4D.h"
+#include "DivideByZeroException.h"
+#include "OverflowException.h"
 
 using namespace std;
-
-#include "Point4D.h"
 
 template <typename T>
 void Point4D<T>::print(ofstream& f) {
@@ -17,6 +18,98 @@ void Point4D<T>::print() {
 	cout << '(' << getX() << ', ' << getY() << ', ' << z4 << ')';
 }
 
+/* + operator method */
+template <typename T>
+Point4D<T> operator+(Point4D<T> p, T scale) {
+	if (p.getX() + scale > numeric_limits<T>::max() || p.getX() + scale < numeric_limits<T>::min()) {
+		throw OverflowException();
+	}
+	return Point4D<T>(p.getX() + scale, p.getY() + scale, p.getZ() + scale, p.z4 + scale);
+}
+
+template <typename T>
+Point4D<T> operator+(Point4D<T> p1, Point4D<T> p2) {
+	if (p1.getX() + p2.getX() > numeric_limits<T>::max() || p1.getX() + p2.getX() < numeric_limits<T>::min()) {
+		throw OverflowException();
+	}
+	return Point4D<T>(p1.getX() + p2.getX(), p1.getY() + p2.getY(), p1.getZ() + p2.getZ(), p1.z4 + p2.z4);
+}
+
+/* - operator method */
+template <typename T>
+Point4D<T> operator-(Point4D<T> p, T scale) {
+	if (p.getX() - scale > numeric_limits<T>::max() || p.getX() - scale < numeric_limits<T>::min()) {
+		throw OverflowException();
+	}
+	return Point4D<T>(p.getX() - scale, p.getY() - scale, p.getZ() - scale, p.z4 - scale);
+}
+
+template <typename T>
+Point4D<T> operator-(Point4D<T> p1, Point4D<T> p2) {
+	if (p1.getX() - p2.getX() > numeric_limits<T>::max() || p1.getX() - p2.getX() < numeric_limits<T>::min()) {
+		throw OverflowException();
+	}
+	return Point4D<T>(p1.getX() - p2.getX(), p1.getY() - p2.getY(), p1.getZ() - p2.getZ(), p1.z4 - p2.z4);
+}
+
+/* * operator method */
+template <typename T>
+Point4D<T> operator*(Point4D<T> p, T scale) {
+	if ((p.getX() > 0) && (p.getX() * scale > numeric_limits<T>::max())) {
+		throw OverflowException();
+	}
+	else if ((p.getX() < 0) && (p.getX() * scale < numeric_limits<T>::min())) {
+		throw OverflowException();
+	}
+	return Point4D<T>(p.getX() * scale, p.getY() * scale, p.getZ() * scale, p.z4 * scale);
+}
+
+template <typename T>
+Point4D<T> operator*(Point4D<T> p1, Point4D<T> p2) {
+	if ((p1.getX() > 0) && (p1.getX() * p2.getX() > numeric_limits<T>::max())) {
+		throw OverflowException();
+	}
+	else if ((p1.getX() < 0) && (p1.getX() * p2.getX() < numeric_limits<T>::min())) {
+		throw OverflowException();
+	}
+	return Point4D<T>(p1.getX() * p2.getX(), p1.getY() * p2.getY(), p1.getZ() * p2.getZ(), p1.z4 * p2.z4);
+}
+
+/* / operator method */
+template <typename T>
+Point4D<T> operator/(Point4D<T> p, T scale) {
+	if (scale == 0) {
+		throw DivideByZeroException();
+	}
+	return Point4D<T>(p.getX() / scale, p.getY() / scale, p.getZ() / scale, p.z4 / scale);
+}
+
+template <typename T>
+Point4D<T> operator/(Point4D<T> p1, Point4D<T> p2) {
+	if ((p2.getX() == 0) || (p2.getY() == 0) || (p2.getZ() == 0) || (p2.z4 == 0)) {
+		throw DivideByZeroException();
+	}
+	return Point4D<T>(p1.getX() / p2.getX(), p1.getY() / p2.getY(), p1.getZ() / p2.getZ(), p1.z4 / p2.z4);
+}
+
 template class Point4D<int>;
 template class Point4D<float>;
 template class Point4D<double>;
+
+/* / operator method */
+// template <typename T>
+// Point4D<T> operator/(Point4D<T> p, T scale) {
+// 	if (scale == 0) {
+// 		throw DivideByZeroException();
+// 	}
+// 	return Point4D<T>(p.getX() / scale, p.getY() / scale, p.getZ() / scale, p.z4 / scale);
+// }
+
+// template <typename T>
+// Point4D<T> operator/(Point4D<T> p1, Point4D<T> p2) {
+// 	if ((p2.getX() == 0) || (p2.getY() == 0) || (p2.getZ() == 0) || (p2.z4 == 0)) {
+// 		throw DivideByZeroException();
+// 	}
+// 	return Point4D<T>(p1.getX() / p2.getX(), p1.getY() / p2.getY(), p1.getZ() / p2.getZ(), p1.z4 / p2.z4);
+// }
+
--- KMITL_C-CPP/Semester_2/Lab8/Point4D.h
+++ KMITL_C-CPP/Semester_2/Lab8/Point4D.h
@@ -10,34 +10,32 @@ using namespace std;
 template <typename T>
 class Point4D : public Point3D {
 private:
-	double z4;
+	T z4;
 public:
-	Point4D(double x, double y, double z, double z4) : Point3D(x, y, z) {
+	Point4D(T x, T y, T z, T z4) : Point3D(x, y, z) {
 		this->z4 = z4;
 	}
 
 	/* + operator method */
-	Point4D<T> operator+(Point4D<T> p) {
-		return Point4D<T>(getX() + p.getX(), getY() + p.getY(), getZ() + p.getZ(), z4 + p.z4);
-	}
+	Point4D<T> operator+(T scale);
+	Point4D<T> operator+(Point4D<T> p);
+	//Point4D<T> operator+(Point4D<T> p1, Point4D<T> p2) { }
 
 	/* - operator method */
-	Point4D<T> operator-(Point4D<T> p) {
-		return Point4D<T>(getX() - p.getX(), getY() - p.getY(), getZ() - p.getZ(), z4 - p.z4);
-	}
+	Point4D<T> operator-(T scale);
+	Point4D<T> operator-(Point4D<T> p);
+	//Point4D<T> operator-(Point4D<T> p1, Point4D<T> p2) { }
 
 	/* * operator method */
-	Point4D<T> operator*(double scale) {
-		return Point4D<T>(getX() * scale, getY() * scale, getZ() * scale, z4 * scale);
-	}
+	Point4D<T> operator*(Point4D<T> p);
+	Point4D<T> operator*(T scale);
+	//Point4D<T> operator*(Point4D<T> p1, Point4D<T> p2) { }
 
 	/* / operator method */
-	Point4D<T> operator/(double scale) {
-		return Point4D<T>(getX() / scale, getY() / scale, getZ() / scale, z4 / scale);
-	}
-
+	Point4D<T> operator/(Point4D<T> p);
+	Point4D<T> operator/(T scale);
+	//Point4D<T> operator/(Point4D<T> p1, Point4D<T> p2) { }
 
 	void print(ofstream& f);
 	void print();
 };
-

cae376e8d149cbdd5c6963e15f0f8aca13e9256a
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-03-25 14:36:30 +0700

    Create QuadrantException class

---
+++ KMITL_C-CPP/Semester_2/Lab8/QuadrantException.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+//user defined class for handling exception
+
+class QuadrantException : public runtime_error {
+public:
+	QuadrantException()
+		: runtime_error("Quadrant error: Point is not in the first quadrant\n") {
+	}
+};

cd965721bb0a1ec06c9555064dcd53ef6518ef98
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-08-10 15:33:30 +0700

    change dir

--- AOOP/lecture01/Welcome1.java
+++
@@ -1,7 +0,0 @@
-//Welcome Java App
-public class Welcome1 {
-	// main method begins execution of Java application
-	public static void main(String[] args) {
-		System.out.println("Welcome to\nJava\nProgramming!");
-	} // end method main
-} // end class Welcome1

bbf9e9c1bcb6217d9e3e210bcb08271b4a9819b3
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-08-31 14:17:23 +0700

    Complete more libft

--- 42-Cursus/Libft/ft_putnbr_fd.c
+++ 42-Cursus/Libft/ft_putnbr_fd.c
@@ -12,4 +12,28 @@
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		write(fd, "-", 1);
+		write(fd, "2", 1);
+		n = 147483648;
+	}
+
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		n %= 10;
+	}
+	if (n < 10)
+	{
+		n += 48;
+		write(fd, &n, 1);
+	}
+}

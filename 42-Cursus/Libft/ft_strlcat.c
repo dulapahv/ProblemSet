244bc10c6bf7103832b9164c21959738cfc885df
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-09-05 11:45:12 +0700

    Complete more libft and fix norm

--- 42-Cursus/Libft/ft_strlcat.c
+++ 42-Cursus/Libft/ft_strlcat.c
@@ -12,4 +12,21 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	c;
+	size_t	d;
+
+	if (dstsize <= ft_strlen(dst))
+		return (dstsize + ft_strlen(src));
+	c = ft_strlen(dst);
+	d = 0;
+	while (src[d] != 0 && c + 1 < dstsize)
+	{
+		dst[c] = src[d];
+		c++;
+		d++;
+	}
+	dst[c] = 0;
+	return (ft_strlen(dst) + ft_strlen(&src[d]));
+}

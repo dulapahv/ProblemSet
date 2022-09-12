6c81ac5714069220c10c218763920caf98aa6493
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-09-12 15:32:09 +0700

    update files

--- 42-Cursus/Libft/ft_memccpy.c
+++ 42-Cursus/Libft/ft_memccpy.c
@@ -3,13 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: duvibuls <duvibuls@student.42bangkok.com>  +#+  +:+       +#+        */
+/*   By: duvibuls <duvibuls@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 20:50:41 by duvibuls          #+#    #+#             */
-/*   Updated: 2022/08/10 20:50:41 by duvibuls         ###   ########.fr       */
+/*   Updated: 2022/09/07 13:25:30 by duvibuls         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*source;
+	unsigned char	*desti;
+
+	i = 0;
+	source = (unsigned char *)src;
+	desti = (unsigned char *)dst;
+	while (i < n)
+	{
+		desti[i] = source[i];
+
+		i++;
+	}
+	return (NULL);
+}

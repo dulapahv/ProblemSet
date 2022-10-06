47b34dbc7c5ddeff6a4ad96d653cec7e4128e085
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-10-06 14:01:34 +0700

    Did some scratch work

---
+++ SCRATCH/Rusty/lab99/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x: i8 = 5;
+    let y: i8 = 0;
+
+    println!("x + y = {}", x + y);
+    println!("x - y = {}", x - y);
+    println!("x * y = {}", x * y);
+    println!("x / y = {}", x / y);
+}

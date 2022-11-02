2f89ca28c9b4c73754f115f1ce199c97e0161c8a
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-11-02 23:05:12 +0700

    complete Linear Algebra HW

--- Linear_Algebra/test.py
+++ Linear_Algebra/test.py
@@ -1,23 +1,47 @@
-# import numpy as np
-
-# e = [1, 2, 3]
-# f = [4, 5, 6]
-
-# v1 = np.array(e)
-# v2 = np.array(f)
-
-# print(v1 + v2)
-
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-red = mcolors.to_rgb([1, 0, 0])
-green = mcolors.to_rgb([0, 1, 0])
-blue = mcolors.to_rgb([0, 0, 1])
 
-plt.plot(2, 4, 'o')
-plt.plot(2, 4, 's')
-plt.plot(2, 4, 'ro')
-plt.plot(2, 4, 'o', color='r', markersize=50)
-plt.plot(2, 4, 'o', color=green, markersize=50)
 
-plt.show()
+def parabola(t, y):
+    """
+    t = [0 1 2 3 4 5 6 7 8 9]
+    t ** 2 = [0 1 4 9 16 25 36 49 64 81]
+
+    np.vstack([t ** 2, t, np.ones(len(t))]) =
+    [[ 0.  1.  4.  9. 16. 25. 36. 49. 64. 81.]
+    [ 0.  1.  2.  3.  4.  5.  6.  7.  8.  9.]
+    [ 1.  1.  1.  1.  1.  1.  1.  1.  1.  1.]]
+
+    np.vstack([t ** 2, t, np.ones(len(t))]).T = 
+    [[ 0.  0.  1.]
+    [ 1.  1.  1.]
+    [ 4.  2.  1.]
+    [ 9.  3.  1.]
+    [16.  4.  1.]
+    [25.  5.  1.]
+    [36.  6.  1.]
+    [49.  7.  1.]
+    [64.  8.  1.]
+    [81.  9.  1.]]
+
+    np.linalg.lstsq(A, y, rcond=None)[0].round(6)
+    array([-4.56515152, 46.13060606, 30.78909091])
+    """
+    A = np.vstack([t ** 2, t, np.ones(len(t))]).T
+    a, b, c = np.linalg.lstsq(A, y, rcond=None)[0].round(6)
+    print(f"y = {c} {'+' if b >= 0 else ''} {b} t {'+' if a >= 0 else ''} {a} t^2")
+
+    plt.plot(t, y, 'o')
+    plt.plot(t, y0 + v0*t - 9.8/2 * t**2, 'r-')
+    plt.show()
+
+
+t = np.arange(10)
+y0 = 20
+v0 = 50
+dy = np.random.randint(-50, 50, 10)/5
+y = y0 + v0*t - 9.8/2 * t**2 + dy
+print(t)
+print(y)
+
+parabola(t, y)

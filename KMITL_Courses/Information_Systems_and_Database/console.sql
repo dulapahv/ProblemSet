fe600bcdc9351786c14838550c2cbab84963bfb0
Dulapah Vibulsanti <dulapah6@gmail.com>
2023-05-05 00:31:25 +0700

    Update

--- KMITL_Courses/Information_Systems_and_Database/console.sql
+++ KMITL_Courses/Information_Systems_and_Database/console.sql
@@ -8,11 +8,11 @@
 -- WHERE "PARTY" = 'Republican'
 
 -- Show the age at death of the president who died the youngest.
--- SELECT min("DEATH_AGE")
+-- SELECT MIN("DEATH_AGE")
 -- FROM "PRESIDENT"
 
 -- Show the age at death of the Democratic president who died the oldest
--- SELECT max("DEATH_AGE")
+-- SELECT MAX("DEATH_AGE")
 -- FROM "PRESIDENT"
 -- WHERE "PARTY" = 'Democratic'
 
@@ -32,13 +32,13 @@
 -- SELECT COUNT(DISTINCT "PRES_NAME")
 -- FROM "PRES_MARRIAGE"
 
--- Show the aerage age at death of the deceased presidents.
+-- Show the average age at death of the deceased presidents.
 -- SELECT SUM("DEATH_AGE") / COUNT(*)
 -- FROM "PRESIDENT"
 -- WHERE "DEATH_AGE" IS NOT NULL
 
 -- Find those deceased politicians who served more than 10% of their lives as presidents. List their names and that
--- ercentage.
+-- percentage.
 -- SELECT "PRES_NAME", 100 * "YRS_SERV" / "DEATH_AGE"
 -- FROM "PRESIDENT"
 -- WHERE (100 * "YRS_SERV" / "DEATH_AGE") > 10
@@ -115,12 +115,12 @@
 
 -- Find those presidents who married at least twice and those maximum number of children in any of their marriages
 -- exceeds their minimum number of children by at least 2. List their names, and the maximum and minimum number of
--- Children.
+-- children.
 -- SELECT "PRES_NAME", MAX("NR_CHILDREN"), MIN("NR_CHILDREN")
 -- FROM "PRES_MARRIAGE"
 -- GROUP BY "PRES_NAME"
 -- HAVING COUNT(*) >= 2
---   AND MAX("NR_CHILDREN") >= MIN("NR_CHILDREN") + 2
+--    AND MAX("NR_CHILDREN") >= MIN("NR_CHILDREN") + 2
 
 --------------------------------------------------------------------------------
 
@@ -146,7 +146,7 @@
 -- SELECT "PRESIDENT"."PRES_NAME", "BIRTH_YR", "HOBBY"
 -- FROM "PRESIDENT", "PRES_HOBBY"
 -- WHERE "PRESIDENT"."PRES_NAME" = "PRES_HOBBY"."PRES_NAME"
---     AND "BIRTH_YR" < 1800
+--   AND "BIRTH_YR" < 1800
 -- ORDER BY "BIRTH_YR", "PRESIDENT"."PRES_NAME"
 
 -- List name, birth year, marriage years, and spouse name of those presidents who were born before 1776 and married
@@ -154,8 +154,8 @@
 -- SELECT "PRESIDENT"."PRES_NAME", "BIRTH_YR", "MAR_YEAR", "SPOUSE_NAME"
 -- FROM "PRESIDENT", "PRES_MARRIAGE"
 -- WHERE "PRESIDENT"."PRES_NAME" = "PRES_MARRIAGE"."PRES_NAME"
---     AND "BIRTH_YR" < 1776
---     AND "MAR_YEAR" < 1800
+--   AND "BIRTH_YR" < 1776
+--   AND "MAR_YEAR" < 1800
 -- ORDER BY "PRESIDENT"."PRES_NAME"
 
 -- List the name, birth year, age at marriage, spouse's age at marriage, and name, for all presidents who married when
@@ -164,7 +164,7 @@
 -- SELECT "PRESIDENT"."PRES_NAME", "BIRTH_YR", "MAR_YEAR", "SP_AGE", "SPOUSE_NAME"
 -- FROM "PRESIDENT", "PRES_MARRIAGE"
 -- WHERE "PRESIDENT"."PRES_NAME" = "PRES_MARRIAGE"."PRES_NAME"
---     AND ("PR_AGE" < 20
+--   AND ("PR_AGE" < 20
 --     OR "SP_AGE" < 18)
 -- ORDER BY "PR_AGE"
 
@@ -177,4 +177,171 @@
 -- HAVING SUM("NR_CHILDREN") > 3
 -- ORDER BY 3 DESC, 1
 
--- Page 72
+-- List all the facts of those presidential marriages which resulted in a number of children that is greater than the
+-- average number of children per presidential marriage.
+-- SELECT *
+-- FROM "PRES_MARRIAGE"
+-- WHERE "NR_CHILDREN" >
+--       (SELECT AVG("NR_CHILDREN")
+--        FROM "PRES_MARRIAGE")
+
+-- Show the name and age of the president who died the youngest
+-- SELECT "PRES_NAME", "DEATH_AGE"
+-- FROM "PRESIDENT"
+-- WHERE "DEATH_AGE" =
+--       (SELECT MIN("DEATH_AGE")
+--        FROM "PRESIDENT")
+
+-- List the hobbies and names of all those presidents who served 8 years or longer. Order by hobbies, presidents name.
+-- SELECT "HOBBY", "PRES_NAME"
+-- FROM "PRES_HOBBY"
+-- WHERE "PRES_NAME" IN
+--       (SELECT "PRES_NAME"
+--        FROM "PRESIDENT"
+--        WHERE "YRS_SERV" >= 8)
+-- ORDER BY 1, 2
+
+-- SELECT "HOBBY", "PRES_NAME"
+-- FROM "PRES_HOBBY"
+-- WHERE "PRES_NAME" = ANY
+--       (SELECT "PRES_NAME"
+--        FROM "PRESIDENT"
+--        WHERE "YRS_SERV" >= 8)
+-- ORDER BY 1, 2
+
+-- SELECT "HOBBY", "PRESIDENT"."PRES_NAME"
+-- FROM "PRESIDENT", "PRES_HOBBY"
+-- WHERE "PRESIDENT"."PRES_NAME" = "PRES_HOBBY"."PRES_NAME"
+--   AND "YRS_SERV" >= 8
+-- ORDER BY 1, 2
+
+-- Which presidents never won an election?
+-- SELECT "PRES_NAME"
+-- FROM "PRESIDENT"
+-- WHERE "PRES_NAME" NOT IN
+--       (SELECT DISTINCT "CANDIDATE"
+--        FROM "ELECTION"
+--        WHERE "WINNER_LOSER_INDIC" = 'W')
+
+-- Which state provided the largest number of presidents, and what is that number?
+-- SELECT "STATE_BORN", COUNT(*)
+-- FROM "PRESIDENT"
+-- GROUP BY "STATE_BORN"
+-- HAVING COUNT(*) >= ALL
+--        (SELECT COUNT(*)
+--         FROM "PRESIDENT"
+--         GROUP BY "STATE_BORN")
+
+-- Find those states which entered the union before President Washington was inaugurated.
+-- SELECT "STATE_NAME"
+-- FROM "STATE"
+-- WHERE "YEAR_ENTERED" < ALL
+--       (SELECT "YEAR_INAUGURATED"
+--        FROM "ADMINISTRATION"
+--        WHERE "PRES_NAME" = 'Washington G')
+
+-- List all the facts available in the table PRESIDENT about those presidents who were inaugurated after Hawaii entered
+-- the union.
+-- SELECT *
+-- FROM "PRESIDENT"
+-- WHERE "PRES_NAME" = ANY
+--       (SELECT "PRES_NAME"
+--        FROM "ADMINISTRATION"
+--        WHERE "YEAR_INAUGURATED" >
+--              (SELECT "YEAR_ENTERED"
+--               FROM "STATE"
+--               WHERE "STATE_NAME" = 'Hawaii'))
+
+-- List the names and ages at death of those presidents who were married more than once. Order by name.
+-- SELECT "PRES_NAME", "DEATH_AGE"
+-- FROM "PRESIDENT"
+-- WHERE "PRES_NAME" IN
+--       (SELECT "PRES_NAME"
+--        FROM "PRES_MARRIAGE"
+--        GROUP BY "PRES_NAME"
+--        HAVING COUNT(*) > 1)
+-- ORDER BY "PRES_NAME"
+
+-- Find those states which entered the union the same year as President Eisenhower was born. Order by state name.
+-- SELECT "STATE_NAME"
+-- FROM "STATE", "PRESIDENT"
+-- WHERE "YEAR_ENTERED" = "BIRTH_YR"
+--   AND "PRES_NAME" = 'Eisenhower D D'
+-- ORDER BY "STATE_NAME"
+
+-- SELECT "STATE_NAME"
+-- FROM "STATE"
+-- WHERE "YEAR_ENTERED" =
+--       (SELECT "BIRTH_YR"
+--        FROM "PRESIDENT"
+--        WHERE "PRES_NAME" = 'Eisenhower D D')
+-- ORDER BY "STATE_NAME"
+
+-- For each president who was born in a year in which at least one other president was born list his name and birth
+-- year.
+-- SELECT "PRES_NAME", "BIRTH_YR"
+-- FROM "PRESIDENT"
+-- WHERE "BIRTH_YR" IN
+--       (SELECT "BIRTH_YR"
+--        FROM "PRESIDENT"
+--        GROUP BY "BIRTH_YR"
+--        HAVING COUNT(*) > 1)
+
+-- List presidents born on same year. List each president only once.
+-- SELECT T1."PRES_NAME", T1."BIRTH_YR", T2."PRES_NAME", T2."BIRTH_YR"
+-- FROM "PRESIDENT" T1, "PRESIDENT" T2
+-- WHERE T1."BIRTH_YR" = T2."BIRTH_YR"
+--   AND T1."PRES_NAME" < T2."PRES_NAME"
+
+-- Using the table ELECTION for all elections after 1900, create a new table with the following columns:
+-- ELECTION YEAR, WINNER, WINNER VOTES, LOSERS, LOSERS VOTES
+-- SELECT W."ELECTION_YEAR", W."CANDIDATE", W."VOTES", L."CANDIDATE", L."VOTES"
+-- FROM "ELECTION" W, "ELECTION" L
+-- WHERE W."ELECTION_YEAR" = L."ELECTION_YEAR"
+--   AND W."ELECTION_YEAR" > 1900
+--   AND W."WINNER_LOSER_INDIC" = 'W'
+--   AND L."WINNER_LOSER_INDIC" = 'L'
+-- ORDER BY W."ELECTION_YEAR", L."VOTES" DESC
+
+-- List election year and winner of those elections in which the winner received more than 80% of the votes of that
+-- election.
+-- SELECT "ELECTION_YEAR", "CANDIDATE"
+-- FROM "ELECTION" E
+-- WHERE "WINNER_LOSER_INDIC" = 'W'
+--   AND "VOTES" >
+--       (SELECT 0.8 * SUM("VOTES")
+--        FROM "ELECTION"
+--        WHERE "ELECTION"."ELECTION_YEAR" = E."ELECTION_YEAR")
+
+-- Select the president's name, birth year and the year of inauguration of his first administration, and order the
+-- result in sequence of year of inauguration
+-- SELECT "PRESIDENT"."PRES_NAME", "BIRTH_YR", "YEAR_INAUGURATED"
+-- FROM "PRESIDENT", "ADMINISTRATION"
+-- WHERE "PRESIDENT"."PRES_NAME" = "ADMINISTRATION"."PRES_NAME"
+--   AND "YEAR_INAUGURATED" =
+--       (SELECT MIN("YEAR_INAUGURATED")
+--        FROM "ADMINISTRATION"
+--        WHERE "PRESIDENT"."PRES_NAME" = "ADMINISTRATION"."PRES_NAME")
+-- ORDER BY "YEAR_INAUGURATED"
+
+-- For those presidents whose number of marriages equals the number of administrations they served as president, list
+-- their name and this number
+-- SELECT "PRES_NAME", COUNT(*)
+-- FROM "PRES_MARRIAGE"
+-- GROUP BY "PRES_NAME"
+-- HAVING COUNT(*) =
+--        (SELECT COUNT(*)
+--         FROM "ADMINISTRATION"
+--         WHERE "ADMINISTRATION"."PRES_NAME" = "PRES_MARRIAGE"."PRES_NAME")
+
+-- Find the names of those presidents born in a state which entered the union not more than 30 years earlier than their
+-- first inauguration year.
+SELECT "PRES_NAME"
+FROM "PRESIDENT" X
+WHERE "STATE_BORN" IN
+      (SELECT "STATE_NAME"
+       FROM "STATE"
+       WHERE "YEAR_ENTERED" + 30 >= ANY
+             (SELECT MIN("YEAR_INAUGURATED")
+              FROM "ADMINISTRATION"
+              WHERE "PRES_NAME" = X."PRES_NAME"))
